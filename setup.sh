27c30638fd16b25ca00852752cec7b088c50e1ec
dagsbirta <dagsbirta@protonmail.sh>
2020-11-15 09:13:28 +0000

    Added new params

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ apt-get install -y git htop vim
 pip install -r requirements.txt
 pip install gdown jupyterlab
 
-# train.py --data=./dataset/endless/ --outdir=./training-runs --snap=5 --cfg=endless --gpus=8 --metrics=None --resume=ffhq512 --mirror=True
+# train.py --data=./datasets/endless/ --outdir=./training-runs --snap=5 --cfg=endless --gpus=2 --metrics=None --resume=ffhq512 --mirror=True
